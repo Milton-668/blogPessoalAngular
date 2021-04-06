0f8e923c58838fb0d87b8c17d048c70765134593
Milton-668 <miltonmjcp@gmail.com>
2021-04-06 01:37:01 -0300

    feat(project): Implementado metodos post e get em postagem

--- src/app/Model/Tema.ts
+++ src/app/Model/Tema.ts
@@ -1,9 +1,9 @@
 import { Postagem } from "./Postagem";
 
 export class Tema{
-    
-    public id: number;
-    public descricao: string;
 
-    public postagem: Postagem[];
+    public id: number
+    public descricao: string
+
+    public postagem: Postagem[]
 }
